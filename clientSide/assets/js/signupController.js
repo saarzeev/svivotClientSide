79cffe1f14203db6096fde07272eb8404f81a50d
סער זאב גוטמן <saarzeev@auth.ad.bgu.ac.il>
2019-06-17 20:05:43 +0300

    Sign up is working

--- clientSide/assets/js/signupController.js
+++ clientSide/assets/js/signupController.js
@@ -1,24 +1,55 @@
-import { error } from "protractor";
 
 // signup controller
 angular.module("myApp")
 .controller("signupController", function ($scope, $http, $location) {
-    $scope.registrationForm = {
-        username: "",
-        psw: "",
-        qa:[{question: "nu1", answer: "nu1Ans"}, {question: "nu2", answer: "nu2Ans"}],
-        email: "",
-        city: "",
-        country: "",
-        firstName: "",
-        lastName: "",
-        categories: ["Food", "Shopping"]
+    // $scope.registrationForm = {
+    //     username: "",
+    //     psw: "",
+    //     qa:[{question: 'What is your mothers maiden name?', answer: ''}, {question: 'What is your hometown?', answer: ''}],
+    //     email: "",
+    //     city: "",
+    //     country: "",
+    //     firstName: "",
+    //     lastName: "",
+    //     categories: $scope.categories
+    // };
+
+    $scope.countries = ['Australia', 'Bolivia', 'China', 'Denemark', 'Israel', 'Latvia', 'Monaco', 'August', 'Norway', 'Panama', 'Switzerland', 'USA'];
+    $scope.countries.sort();
+
+    // $scope.categories = ['Food', 'Shopping', 'Museums', 'Churches'];
+    // $scope.categories.sort();
+
+    
+
+    $scope.isSelectionInvalid = function(registrationForm) {
+        if(registrationForm.country == ""){
+            alert("Please select a country");
+        }else if(registrationForm.categories.length < 2){
+            alert("Please select at least 2 categories you are interested in");
+        }
+        return(registrationForm.country == "" || registrationForm.categories.length < 2 );
     };
 
     $scope.submitSignupForm = function () {
         const url = `${localUrl}/signup`;
-        
-        $http.post(url, $scope.registrationForm).then($scope.successfullSignUp, $scope.errorOnSignUp);
+        if(!this.isSelectionInvalid()){
+
+            $scope.registrationForm = {
+                username: $scope.username,
+                psw: $scope.psw,
+                qa:[{question: 'What is your mothers maiden name?', answer: $scope.ans1}, {question: 'What is your hometown?', answer: $scope.ans2}],
+                email: $scope.email,
+                city: $scope.city,
+                country: $scope.country,
+                firstName: $scope.firstName,
+                lastName: $scope.lastName,
+                categories: $scope.categories
+            };
+
+            if(!this.isSelectionInvalid($scope.registrationForm))
+            $http.post(url, $scope.registrationForm).then($scope.successfullSignUp, $scope.errorOnSignUp);
+        }
     };
 
     $scope.successfullSignUp = function(response) {
