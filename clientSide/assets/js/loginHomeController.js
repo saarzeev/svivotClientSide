a64410e44f5f81a62b7ee6ba15a00f057522a7a3
anastasiakova <kovalkov@post.bgu.ac.il>
2019-06-19 21:37:24 +0300

    all in loginhome

--- clientSide/assets/js/loginHomeController.js
+++ clientSide/assets/js/loginHomeController.js
@@ -98,25 +98,105 @@ angular.module("myApp")
             }
         }
 
-        // When the user clicks on the button, open the modal 
-        $scope.onModalClick = function(index, modalName){
-            var modal = document.getElementById(`${modalName}${index}`);
-            modal.style.display = "block";
+        $scope.submitReview = function (poi) {
+            const url = `${localUrl}/logged/addReview`;
+            const headers = { headers: { "x-auth-token": $window.sessionStorage.token } };
+            var data = null;
+            if (this.writtenReview != "") {
+                data = {
+                    poiName: poi.name,
+                    review: this.writtenReview,
+                    rank: parseInt(this.stars, 10),
+                    date: new Date()
+                };
+            }
+            else {
+                data = {
+                    poiName: poi.name,
+                    rank: parseInt(this.stars, 10),
+                    date: new Date()
+                };
+            }
+            $http.post(url, data, headers)
+                .then(() => $scope.successfulReviewAdded(poi))
+                .then(() => $scope.setNewRank(poi))
+                .catch(() => $scope.errorOnAddReview());
+        };
+
+        $scope.successfulReviewAdded = function (poi) {
+            alert("Successfully added your review. Thank you!");
         }
+
+        $scope.errorOnAddReview = function (errorResponse) {
+            if (errorResponse && (errorResponse.status == 404 || errorResponse.status == 400 || errorResponse.status == 500)) {
+                alert("Well, This is embarrassing.\nWe were not able to save your review.\n" + errorResponse.data);
+            }
+        };
+
+        $scope.setNewRank = function (poi) {
+            const url = `${localUrl}/getAllPOI`;
+            $http.get(url)
+                .then((pois) => {
+                    if (pois && pois.data) {
+                        poi.rank = pois.data.find((item) => poi.name === item.name).rank;
+                    }
+                })
+        }
+
+        $scope.addViewToPoi = function (poiName) {
+            const url = `${localUrl}/addView`;
+            const data = { poiName: poiName };
+            return $http.post(url, data);
+        }
+
+        $scope.getTwoLastReviews = function (poiName) {
+            const url = `${localUrl}/getLastReviews`;
+            const data = { poiName: poiName };
+            return $http.post(url, data);
+        }
+
+        $scope.successfulGetTwoLastReviews = function (poi, reviews) {
+            let review = '';
+            if (reviews && reviews.data) {
+                if (reviews.data.length == 1) {
+                    review = `Last Review: ${reviews.data[0].review}`
+                }
+                else if (reviews.data.length == 2) {
+                    review = `Last Reviews: 1. ${reviews.data[0].review}
+                                           2. ${reviews.data[1].review}`;
+                }
+            }
+            poi.reviews = review;
+        }
+
+        // When the user clicks on the button, open the modal 
+        $scope.onModalClick = function (poi) {
+            var modal = document.getElementById("myModal" + poi.name);
+            $scope.getTwoLastReviews(poi.name)
+                .then((reviews) => $scope.successfulGetTwoLastReviews(poi, reviews))
+                .catch(() => poi.reviews = '')
+                .finally(() => modal.style.display = "block");
+        };
 
         // When the user clicks on <span> (x), close the modal
-        $scope.onXClick = function(index, modalName){
-            var modal = document.getElementById(`${modalName}${index}`);
+        $scope.onXClick = function (poi) {
+            var modal = document.getElementById("myModal" + poi.name);
+            $scope.addViewToPoi(poi.name)
+                .then(() => {
+                    poi.views++;
+                })
+                .finally(() => modal.style.display = "none");
+        };
+
+        // When the user clicks on the button, open the modal 
+        $scope.onWriteReviewClick = function (poi) {
+            var modal = document.getElementById("myReviewModal" + poi.name);
+            modal.style.display = "block";
+        };
+
+        // When the user clicks on <span> (x), close the modal
+        $scope.onReviewXClick = function (poi) {
+            var modal = document.getElementById("myReviewModal" + poi.name);
             modal.style.display = "none";
-        }
-
-        // When the user clicks anywhere outside of the modal, close it
-        // window.onclick = function(index) {
-        //     var modal = document.getElementById("myModal" + index);
-
-        //     if (event.target == modal) {
-        //         modal.style.display = "none";
-        //     }
-        // }
-
+        };
     }]);
