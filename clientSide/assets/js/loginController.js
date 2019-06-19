68a2ea104c63316060384c5e04b8dcf8dd546e1a
סער זאב גוטמן <saarzeev@auth.ad.bgu.ac.il>
2019-06-19 10:32:14 +0300

    popular on loged in and favorites get

--- clientSide/assets/js/loginController.js
+++ clientSide/assets/js/loginController.js
@@ -3,98 +3,115 @@ const localUrl = 'http://localhost:3000';
 angular.module("myApp")
 
 
-.controller("loginController", ['$scope','$rootScope','$http', '$window', '$location', function ($scope, $rootScope, $http, $window, $location) {
-    
-    $scope.qa = [{question: 'What is your mothers maiden name?', answer: $scope.ans1}, {question: 'What is your hometown?', answer: $scope.ans2}];
-    $scope.isRestoringPassword = false;
-    $scope.restoredPassword = "";
-    
-    $scope.tryLogIn = function() {
-        const url = `${localUrl}/LogIn`;
-        const data = {username: $scope.loginUserName, psw: $scope.loginPassword };
-        $http.post(url, data).then($scope.successfulLogIn, $scope.errorOnLogIn);
-    };
+    .controller("loginController", ['$scope', '$rootScope', '$http', '$window', '$location', function ($scope, $rootScope, $http, $window, $location) {
 
-    $scope.successfulLogIn = function(response) {
-        if(response && response.data && response.data.token && response.data.name){
-            $window.sessionStorage.setItem('token', response.data.token);
-            $window.sessionStorage.setItem('username', response.data.name);
-            $rootScope.loggedInUsername = $window.sessionStorage.username;
-            $rootScope.welcomePath = '#!loginHome'
-            $rootScope.isLoggedIn = true;
-            $location.url("/loginHome");
+        $scope.qa = [{ question: 'What is your mothers maiden name?', answer: $scope.ans1 }, { question: 'What is your hometown?', answer: $scope.ans2 }];
+        $scope.isRestoringPassword = false;
+        $scope.restoredPassword = "";
+
+        $scope.tryLogIn = function () {
+            const url = `${localUrl}/LogIn`;
+            const data = { username: $scope.loginUserName, psw: $scope.loginPassword };
+            $http.post(url, data).then($scope.successfulLogIn, $scope.errorOnLogIn);
+        };
+
+        $scope.successfulLogIn = function (response) {
+            if (response && response.data && response.data.token && response.data.name) {
+                $window.sessionStorage.setItem('token', response.data.token);
+                $window.sessionStorage.setItem('username', response.data.name);
+                $rootScope.loggedInUsername = $window.sessionStorage.username;
+                $rootScope.welcomePath = '#!loginHome'
+                $rootScope.isLoggedIn = true;
+                $location.url("/loginHome");
+                $scope.getAllMyFavorite();
+            }
+            else {
+                $scope.errorOnLogIn("");
+            }
+        };
+
+        $scope.errorOnLogIn = function (errorResponse) {
+            if (errorResponse && (errorResponse.status == 404 || errorResponse.status == 400)) {
+                $scope.errors = [{ key: 'badParams', value: errorResponse.data }];
+            }
+            else {
+                $scope.errors = [{ key: 'badParams', value: 'We encountered a temporary problem.\nPlease try again later' }];
+            }
+
+        };
+
+        $scope.submitRestorePasswordForm = function () {
+            const url = `${localUrl}/answersIdentificationQuestion`;
+            var locQa = [{ question: 'What is your mothers maiden name?', answer: $scope.ans1 }, { question: 'What is your hometown?', answer: $scope.ans2 }];
+            const data = { username: $scope.username, qa: locQa };
+            $http.post(url, data).then($scope.successRestore, $scope.errorRestore);
+        };
+
+        $scope.successRestore = function (response) {
+            if (response && response.data && response.data.psw) {
+                alert("Password was retrieved successfully! Your password:\n" + response.data.psw);
+                $scope.isRestoringPassword = true;
+            }
+            else {
+                $scope.errorRestore(response);
+            }
+        };
+
+        $scope.errorRestore = function (errorResponse) {
+            if (errorResponse && (errorResponse.status == 404 || errorResponse.status == 400 || errorResponse.status == 500)) {
+                alert("Well, This is embarrassing.\nWe were not able to restore your password.\n" + errorResponse.data);
+            }
+
+        };
+
+        $scope.getAllMyFavorite = function () {
+            const url = `${localUrl}/logged/getFavoritePOI`;
+            const headers = { headers: { "x-auth-token": $window.sessionStorage.token } }
+            $http.post(url, null, headers)
+                .then((favorites) => {
+                    if (favorites && favorites.data) {
+                        $window.sessionStorage.setItem('userFavoritePoi', favorites.data);
+                    }
+                    else {
+                        $scope.errorFavorite();
+                    }
+                }).catch($scope.errorFavorite());
         }
-        else{
-            $scope.errorOnLogIn("");
-        }
-    };
 
-    $scope.errorOnLogIn = function(errorResponse) {
-        if(errorResponse && (errorResponse.status == 404 || errorResponse.status == 400)){
-           $scope.errors =  [{ key: 'badParams', value: errorResponse.data }];
-        }
-        else{
-            $scope.errors =  [{ key: 'badParams', value: 'We encountered a temporary problem.\nPlease try again later' }];
-        }
-             
-    };
+        $scope.errorFavorite = function () {
+            $window.sessionStorage.setItem('userFavoritePoi', []);
+        };
+    }])
+    // this is our directive
+    .directive('qnValidate', [function () {
+        return {
+            link: function (scope, element, attr) {
+                var form = element.inheritedData('$formController');
+                // no need to validate if form doesn't exists
+                if (!form) return;
+                // validation model
+                var validate = attr.qnValidate;
+                // watch validate changes to display validation
+                scope.$watch(validate, function (errors) {
 
+                    // every server validation should reset others
+                    // note that this is form level and NOT field level validation
+                    form.$serverError = {};
 
-    $scope.submitRestorePasswordForm = function(){
-        const url = `${localUrl}/answersIdentificationQuestion`;
-        var locQa =  [{question: 'What is your mothers maiden name?', answer: $scope.ans1}, {question: 'What is your hometown?', answer: $scope.ans2}];
-        const data = {username: $scope.username, qa: locQa };
-        $http.post(url, data).then($scope.successRestore, $scope.errorRestore);
-    };
+                    // if errors is undefined or null just set invalid to false and return
+                    if (!errors) {
+                        form.$serverInvalid = false;
+                        return;
+                    }
+                    // set $serverInvalid to true|false
+                    form.$serverInvalid = (errors.length > 0);
 
-    $scope.successRestore = function(response) {
-        if(response && response.data && response.data.psw){
-            alert("Password was retrieved successfully! Your password:\n" + response.data.psw);
-            $scope.isRestoringPassword = true;
-        }
-        else{
-            $scope.errorRestore(response);
-        }
-    };
-
-    $scope.errorRestore = function(errorResponse) {
-        if(errorResponse && (errorResponse.status == 404 || errorResponse.status == 400 || errorResponse.status == 500)){
-            alert("Well, This is embarrassing.\nWe were not able to restore your password.\n" + errorResponse.data);
-        }
-             
-    };
-    
-}])
-// this is our directive
-.directive('qnValidate', [function() {
-    return {
-        link: function(scope, element, attr) {
-            var form = element.inheritedData('$formController');
-            // no need to validate if form doesn't exists
-            if (!form) return;
-            // validation model
-            var validate = attr.qnValidate;
-            // watch validate changes to display validation
-            scope.$watch(validate, function(errors) {
-
-                // every server validation should reset others
-                // note that this is form level and NOT field level validation
-                form.$serverError = { };
-
-                // if errors is undefined or null just set invalid to false and return
-                if (!errors) {
-                    form.$serverInvalid = false;
-                    return;
-                }
-                // set $serverInvalid to true|false
-                form.$serverInvalid = (errors.length > 0);
-
-                // loop through errors
-                angular.forEach(errors, function(error) {                            
+                    // loop through errors
+                    angular.forEach(errors, function (error) {
                         form.$serverError[error.key] = { $invalid: true, message: error.value };
+                    });
                 });
-            });
-        }
-    };
-}]);
+            }
+        };
+    }]);
 
--- clientSide/assets/js/loginHomeController.js
+++ clientSide/assets/js/loginHomeController.js
@@ -1,66 +1,78 @@
 angular.module("myApp")
 
-.controller("loginHome", ['$scope','$http', '$window', function ($scope, $http, $window) {
-    
-    $scope.getUsersCategories = function() {
-        const url = `${localUrl}/logged/getUsersCategories`;
-        const headers = { headers: { "x-auth-token": $window.sessionStorage.token } }
-        $http.post(url, null, headers).then($scope.successfulGet, $scope.errorOnGet);
-    }
-    
-    $scope.successfulGet = function(response) {
-        if(response && response.data && response.data.length >= 2) {
-            $scope.setMostPopularPoiByCategory(response.data);
-        }
-        else{
-            $scope.errorOnLogIn("");
-        }
-    }
+    .controller("loginHome", ['$scope', '$http', '$window', '$rootScope', '$location', function ($scope, $http, $window, $rootScope, $location) {
 
-    $scope.errorOnGet = function(errorResponse) {
-        if(errorResponse && errorResponse.status == 400){
-            $scope.error = true;
-            $scope.errorValue = { key: 'badParams', value: errorResponse.data };
-        }
-        if(errorResponse && errorResponse.status == 401){
-            $scope.error = true;
-            $scope.errorValue = { key: 'Unauthorized', value: 'Unauthorized user' };
-        }
-        else {
-            $scope.error = true;
-            $scope.errorValue =  { key: 'noData', value: 'no data to show' };
-        }
-    }
-
-    $scope.getMostPopularByCategory= function(category) {
-        const url = `${localUrl}/getMostPopularByCategory`;
-        const data = {category: category};
-        return $http.post(url, data);
-    }
-
-    $scope.setMostPopularPoiByCategory = function(categories){
-        let firstPois, seconedPois;
-        $scope.getMostPopularByCategory(categories[0].name)
-        .then((firstResponse) => {
-            if(firstResponse && firstResponse.data){
-                $scope.getMostPopularByCategory(categories[1].name);
-                firstPois = firstResponse.data;
+        // popular functions
+        $scope.getUsersCategories = function () {
+            if (!$rootScope.isLoggedIn) {
+                $location.url('/');
+                $rootScope.welcomePath = '#!/';
+                return;
             }
-            else{
+            const url = `${localUrl}/logged/getUsersCategories`;
+            const headers = { headers: { "x-auth-token": $window.sessionStorage.token } }
+            $http.get(url, headers).then($scope.successfulGet, $scope.errorOnGet);
+        }
+
+        $scope.successfulGet = function (response) {
+            if (response && response.data && response.data.length >= 2) {
+                $scope.setMostPopularPoiByCategory(response.data);
+            }
+            else {
                 $scope.errorOnLogIn("");
             }
-        })
-        .then((seconedResponse) => {
-            if(firstResponse && firstResponse.data){
-                seconedPois = seconedResponse;
-                $scope.byCategoryPois = [firstPois[0], seconedPois[0]];
+        }
+
+        $scope.getMostPopularByCategory = function (category) {
+            const url = `${localUrl}/getMostPopularByCategory`;
+            const data = { category: category };
+            return $http.post(url, data);
+        }
+
+        $scope.setMostPopularPoiByCategory = function (categories) {
+            let firstPois, seconedPois;
+            $scope.getMostPopularByCategory(categories[0].category)
+                .then((firstResponse) => {
+                    if (firstResponse && firstResponse.data) {
+                        firstPois = firstResponse.data;
+                        return $scope.getMostPopularByCategory(categories[1].category);
+                    }
+                    else {
+                        $scope.errorOnLogIn("");
+                    }
+                })
+                .then((seconedResponse) => {
+                    if (seconedResponse && seconedResponse.data) {
+                        seconedPois = seconedResponse.data;
+                        $scope.byCategoryPois = [firstPois[0], seconedPois[0]];
+                    }
+                    else {
+                        $scope.errorOnLogIn("");
+                    }
+                })
+                .catch((err) => {
+                    $scope.errorOnGet(err);
+                })
+        }
+
+        // favorites handler
+
+        
+
+        // errors handler
+        $scope.errorOnGet = function (errorResponse) {
+            if (errorResponse && errorResponse.status == 400) {
+                $scope.error = true;
+                $scope.errorValue = { key: 'badParams', value: errorResponse.data };
             }
-            else{
-                $scope.errorOnLogIn("");
+            if (errorResponse && errorResponse.status == 401) {
+                $scope.error = true;
+                $scope.errorValue = { key: 'Unauthorized', value: 'Unauthorized user' };
             }
-        })
-        .catch((err) => {
-            $scope.errorOnGet(err);
-        })
-    }
-}]);
+            else {
+                $scope.error = true;
+                $scope.errorValue = { key: 'noData', value: 'no data to show' };
+            }
+        }
+
+    }]);
